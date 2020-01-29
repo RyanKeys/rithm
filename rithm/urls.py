deb3337310456b99eae68d624225fd3b6bb8670b
Ryan Keys <r.keys1998@gmail.com>
2020-01-28 20:47:06 -0800

    init app for landing page

--- rithm/urls.py
+++ rithm/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("landing_page.urls")),
     path("note_identification/", include('note_identification.urls')),
     path('admin/', admin.site.urls),
 ]
