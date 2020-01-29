6b47851edc3ad40f265fbea8f5e102ed715be9ca
Ryan Keys <r.keys1998@gmail.com>
2020-01-29 12:10:17 -0800

    added Procfile

--- note_identification/views.py
+++ note_identification/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Note migration test view!")
-# Create your views here.
+    return HttpResponse("Note identification test view!")
