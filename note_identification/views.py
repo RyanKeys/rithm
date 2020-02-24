5b81de40a087a5c7230c1d0d2311e94baa58ab8c
John Miner <john.douglas.miner.iv@gmail.com>
2020-02-24 10:31:25 -0800

    static

--- note_identification/views.py
+++ note_identification/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
+from django.views import generic, View
+from datetime import datetime
+# from .models import Game
 
-def index(request):
-    return HttpResponse("Note identification test view!")
+
+class IndexView(generic.ListView):
+    template_name = 'note_identification/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
